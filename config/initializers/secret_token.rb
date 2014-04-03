502ff10b251e12fd1c714c11036e79e7df645966
originalblend337 <originalblend337@gmail.com>
2014-04-03 12:26:16 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GifOfMusic::Application.config.secret_key_base = 'c38e10bb2a0aaadbb1492cae999e112293f86af261365eea708c1d59fcaf561a99a776230a80049fcdec4f9873fd90d9660039a849383f1d217907e4ab32a4e5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GifOfMusic::Application.config.session_store :cookie_store, key: '_gif_of_music_session'
